32581be1456039b791b828403e0db98d662f5810
Koichi Namasu <kouichi.namasu.rn@nttdocomo.com>
2023-11-28 15:57:45 +0900

    途中経過の保存

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,52 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
         body: Center(
-            child: Container(
-                height: 100,
-                color: Colors.yellow,
-                child: const Center(
-                  child: Column(
-                    children: [
-                      Text('Hello World!'),
-                      Text('こんにちは世界'),
-                      Icon(Icons.star, color: Colors.red),
-                    ],
-                  ),
-                ))),
+          child: [titleSection],
+        ),
       ),
     );
   }
