96fbe2651aa3b975227ebf9adc1c2fb3f8be5fd0
Koichi Namasu <kouichi.namasu.rn@nttdocomo.com>
2023-11-29 13:22:36 +0900

    Step4追加

--- lib/main.dart
+++ lib/main.dart
@@ -47,12 +47,12 @@ class MainApp extends StatelessWidget {
       ),
     );
 
-    Column buildButtonColumn(Color color, Icondata icon, String label) {
+    Column buildButtonColumn(Color color, IconData icon, String label) {
       return Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            icon(icon, color: color),
+            Icon(icon, color: color),
             Container(
                 margin: const EdgeInsets.only(top: 8),
                 child: Text(
@@ -75,14 +75,34 @@ class MainApp extends StatelessWidget {
       buildButtonColumn(color, Icons.share, 'SHARE'),
     ]);
 
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(children: [
+        body: ListView(children: [
+          Image.asset(
+            'images/lake.jpg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
           titleSection,
           buttonSection,
+          textSection,
         ]),
       ),
     );
